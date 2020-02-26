e80475874bb7a3f3ca3345a88c9cd06553df63eb
Lohitapallanti <40202055+Lohitapallanti@users.noreply.github.com>
2020-02-26 10:37:00 +0100

    Add files via upload

---
+++ project/src/layouts/MyLayout.vue
@@ -0,0 +1,118 @@
+<template>
+<q-app>
+  <q-layout>
+<q-content id="top">
+    <div class="bg-img">
+      <!-- <q-header> -->
+      <div class="logo">
+        <q-img src="statics/background logo.png"></q-img>
+      </div>
+     </div>
+    <!--<q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-2"
+    >
+      <q-list>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="icon-ku" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Program 2020</q-item-label>
+            <q-item-label caption>Hva skjer på festivalen?</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://github.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="code" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Billetter</q-item-label>
+            <q-item-label caption>Sikre din plass nå!</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://chat.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="chat" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Spilletider</q-item-label>
+            <q-item-label caption>Når spiller de?</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://forum.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="info" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Info</q-item-label>
+            <q-item-label caption>Nyttig informasjon</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://twitter.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="rss_feed" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Twitter</q-item-label>
+            <q-item-label caption>@quasarframework</q-item-label>
+          </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://facebook.quasar.dev">
+          <q-item-section avatar>
+            <q-icon name="public" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Facebook</q-item-label>
+            <q-item-label caption>@QuasarFramework</q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>-->
+<q-page-container>
+<router-view />
+    </q-page-container>
+    </q-content>
+  </q-layout>
+  </q-app>
+ </template>
+
+<script>
+export default {
+  name: 'MyLayout',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#top,
+#bottom {
+  position: fixed;
+  left: 0;
+  right: 0;
+  height: 50%;
+}
+#top {
+  top: 0;
+}
+.bg-img {
+  position: relative;
+  width: 100%;
+  height: 100%;
+background-size: cover;
+ background-repeat: no-repeat;
+  background-position:50% 50%;
+  background-image: url("../statics/background image.jpg");
+}
+.logo {
+  position: relative;
+  max-width: 300px;
+}
+</style>
---
+++ project/src/pages/index.vue
@@ -0,0 +1,80 @@
+<template>
+<q-page>
+  <q-list>
+    <div id="bottom">
+    <q-item clickable tag="a" target="_blank" href="https://quasar.dev" class="q-pa-md">
+  <q-item-section>
+    <div class="row program">
+          <q-item-label class="col">Program 2020</q-item-label>
+       </div>
+       </q-item-section>
+        </q-item>
+        <q-item clickable tag="a" target="_blank" href="https://quasar.dev" class="q-pa-md">
+  <q-item-section>
+    <div class="row">
+      <div class="col-6">
+        <q-item-label class="ml-2 belooo">Billetter</q-item-label>
+        </div>
+          <div class="col-6">
+             <q-item-label class="ml-2 belooo">Spilletider</q-item-label>
+            </div>
+         </div>
+         </q-item-section>
+         </q-item>
+          <q-item clickable tag="a" target="_blank" href="https://quasar.dev" class="q-pa-md">
+<q-item-section>
+   <div class="row">
+     <div class="col-6">
+        <q-item-label class="ml-2 belooo">Før og etter</q-item-label>
+       </div>
+        <div class="col-6">
+           <q-item-label class="ml-2 belooo">Info</q-item-label>
+          </div>
+     </div>
+  </q-item-section>
+</q-item>
+ </div>
+ </q-list>
+</q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.ml-2 {
+  margin-left: 8px;
+}
+.belooo {
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  background: white;
+}
+#top,
+#bottom {
+position: fixed;
+  left: 0;
+  right: 0;
+  height: 50%;
+}
+#bottom {
+  bottom: 0;
+  background-color: black;
+}
+.program {
+  background: white;
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+}
+</style>
